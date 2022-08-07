69babdf0473eed5f6c77628b9d9e74c5a8a1457b
Juan Alba Harrington <juan.harrington@zeneducate.com>
2022-08-07 15:12:50 -0300

    New api token, moved to .env

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 const Client = {
   getWeatherInfo: (cityName, shortCountryName) => {
     return fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${shortCountryName}&appid=ce5e2c716e02e7844a6da3ea1ee9edb1`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${shortCountryName}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
     ).then((response) => response.json());
   },
 };
